ccad25fa7cf442b7098962650d3aa9fa4a7f2314
LenartA97 <97lenartadam@gmail.com>
2024-05-20 22:09:57 +0200

    kezdetleges frontend, majdnem kész backend

---
+++ src/Post.js
@@ -0,0 +1,13 @@
+export default function Post() {
+    return (
+        <div className="post">
+        <div className="image">
+          <img src="https://image.api.playstation.com/vulcan/ap/rnd/202009/2814/GGyEnCkIXoyiVlN9sRHkzUPo.png" alt=""/>
+        </div>
+        <div className="texts">
+          <h2>DOOM 2016</h2>
+          <p>You’ve come here for a reason. The Union Aerospace Corporation’s massive research facility on Mars is overwhelmed by fierce and powerful demons, and only one person stands between their world and ours. As the lone DOOM Marine, you’ve been activated to do one thing – kill them all.</p>
+        </div>
+      </div>
+    )
+}
